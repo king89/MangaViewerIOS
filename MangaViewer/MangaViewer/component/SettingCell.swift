0f68a73e4f9a081f834b5fd4413b38b7e3e6098a
King <king@Haohuas-MacBook-Air.local>
2016-11-26 13:27:27 -0500

    add pager and menu

---
+++ MangaViewer/MangaViewer/component/SettingCell.swift
@@ -0,0 +1,9 @@
+//
+//  SettingCell.swift
+//  MangaViewer
+//
+//  Created by King on 11/10/16.
+//  Copyright © 2016 king. All rights reserved.
+//
+
+import Foundation
---
+++ MangaViewer/MangaViewer/viewcontroller/MenuDrawerViewControll.swift
@@ -0,0 +1,9 @@
+//
+//  MenuDrawerViewControll.swift
+//  MangaViewer
+//
+//  Created by King on 11/3/16.
+//  Copyright © 2016 king. All rights reserved.
+//
+
+import Foundation
